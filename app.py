49befe77025c546eade8f25e11060cd312a2655b
Univercraft <160036881+Univercraft@users.noreply.github.com>
2025-03-04 11:09:01 +0100

    5

--- app.py
+++ app.py
@@ -5,20 +5,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('templates/acceuil.html')
+    return render_template('acceuil.html')
 
 @app.route('/profile')
 def profile():
     user = {
-        'photo_profil': 'src/images/profile_icon_default.png',
+        'photo_profil': 'static/src/images/profile_icon_default.png',
         'nom': 'Doe',
         'prenom': 'John',
         'email': 'john.doe@example.com',
         'statut': 'Membre'
     }
     badges = [
-        {'image_url': 'src/images/badge1.png', 'nom': 'Badge 1'},
-        {'image_url': 'src/images/badge2.png', 'nom': 'Badge 2'}
+        {'image_url': 'static/src/images/badge1.png', 'nom': 'Badge 1'},
+        {'image_url': 'static/src/images/badge2.png', 'nom': 'Badge 2'}
     ]
     return render_template('profile.html', user=user, badges=badges)
 
