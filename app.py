eee050ffca1ab91f5cf00624cfc550e4acfeafa7
Univercraft <160036881+Univercraft@users.noreply.github.com>
2025-03-04 11:10:30 +0100

    5

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 import os
 
 app = Flask(__name__)
@@ -17,14 +17,25 @@ def profile():
         'statut': 'Membre'
     }
     badges = [
-        {'image_url': 'static/src/images/badge1.png', 'nom': 'Badge 1'},
-        {'image_url': 'static/src/images/badge2.png', 'nom': 'Badge 2'}
+        {'image_url': 'static/src/images/badge_graine_de_champion.png', 'nom': 'Badge 1'},
+        {'image_url': 'static/src/images/badge_gardien_de_potager.png', 'nom': 'Badge 2'}
     ]
     return render_template('profile.html', user=user, badges=badges)
 
 @app.route('/acceuil')
 def acceuil():
     return render_template('acceuil.html')
+
+
+@app.route('/edit_profile')
+def edit_profile():
+    # Logique pour la page de modification du profil
+    return "Page de modification du profil"
+
+@app.route('/logout')
+def logout():
+    # Logique pour la déconnexion
+    return "Déconnexion réussie"
 
 if __name__ == '__main__':
     app.run(debug=True)
